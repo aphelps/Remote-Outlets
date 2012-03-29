4fd747af63d58954ba7f0abf5f8834f18a39b680
Adam Phelps <amp@cs.stanford.edu>
2012-03-29 00:35:51 -0700

    * Added button actions * Added LCD disabling

--- Remote_Control/Buttons.ino
+++ Remote_Control/Buttons.ino
@@ -1,40 +1,82 @@
-int buttonState[NUM_BUTTONS];
-int lastState[NUM_BUTTONS];
-long debounceTime[NUM_BUTTONS];
+/*
+ * Code for on-off controls
+ */
 
-void debounce_init() {
-  for (int i = 0; i < NUM_BUTTONS; i++) {
-    buttonState[i] = LOW;
-    lastState[i] = LOW;
-    debounceTime[i] = 0;
+typedef void (*button_action_t)(int button, int value);
+
+#define MAX_BUTTONS 4
+int num_buttons = 0;
+int buttons_pin[MAX_BUTTONS];
+int buttons_curr_state[MAX_BUTTONS];
+int buttons_prev_state[MAX_BUTTONS];
+long buttons_debounce_time[MAX_BUTTONS];
+button_action_t buttons_action[MAX_BUTTONS];
+
+void action_set_lcd(int button, int value) 
+{
+  String text = String(String("button ") + String(button) + String(":") + String(value));
+  LCD_set(button - 1, 0, text);
+}
+
+void action_light_led(int button, int value) 
+{
+  digitalWrite(LED, value);
+}
+
+void buttons_set_action(int button, void (*action)(int button, int value)) 
+{
+  buttons_action[button] = action;
+}
+
+
+void buttons_init(const int buttons[], const int nbuttons) 
+{
+  num_buttons = nbuttons;
+  for (int i = 0; i < num_buttons; i++) {
+    buttons_pin[i] = buttons[i];
+
+    /* Set the pin for this button */
+    pinMode(buttons_pin[i], INPUT);
+
+    /* Initialize debounce states */
+    buttons_curr_state[i] = LOW;
+    buttons_prev_state[i] = LOW;
+    buttons_debounce_time[i] = 0;
+
+    /* Initialize button action */
+    //buttons_action[i] = action_set_lcd;
+    buttons_set_action(i, action_set_lcd);
   }
 }
+
   
 int debounced_read(int button, int debounce_delay) {
-  int currentValue = digitalRead(BUTTONS[button]);
+  int currentValue = digitalRead(buttons_pin[button]);
   
-  if (currentValue != lastState[button]) {
-    debounceTime[button] = millis();
+  if (currentValue != buttons_prev_state[button]) {
+    buttons_debounce_time[button] = millis();
     DEBUG_PRINT(2, "Button ");
     DEBUG_PRINT(2, button);
-    DEBUG_PRINT(2, ": debounceTime=");
-    DEBUG_PRINT(2, debounceTime[button]);
+    DEBUG_PRINT(2, ": buttons_debounce_time=");
+    DEBUG_PRINT(2, buttons_debounce_time[button]);
     DEBUG_PRINT(2, "\n");
   }
-  lastState[button] = currentValue;
+  buttons_prev_state[button] = currentValue;
   
-  if (currentValue != buttonState[button]) {
-    if ((millis() - debounceTime[button]) > debounce_delay) {
-      buttonState[button] = currentValue;
+  if (currentValue != buttons_curr_state[button]) {
+    if ((millis() - buttons_debounce_time[button]) > debounce_delay) {
+      buttons_curr_state[button] = currentValue;
       DEBUG_PRINT(2, "Button ");
       DEBUG_PRINT(2, button);
       DEBUG_PRINT(2, ": value=");
-      DEBUG_PRINT(2, buttonState[button]);
+      DEBUG_PRINT(2, buttons_curr_state[button]);
       DEBUG_PRINT(2, "\n");
+
+      if (buttons_action[button]) buttons_action[button](button, currentValue);
     }
   }
   
-  return buttonState[button];
+  return buttons_curr_state[button];
 }
 
 
--- Remote_Control/LCD.ino
+++ Remote_Control/LCD.ino
@@ -3,16 +3,38 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(5, 6, 7, 8, 9, 10);
 
-
+long lcd_update_time = 0;
+char lcd_enabled = 0;
+long lcd_disable_timeout = 10000;
 
 void LCD_setup() 
 {
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
+  lcd.noCursor();
+  lcd_enabled = 0;
+  lcd.noDisplay();
 }
 
 void LCD_set(int row, int col, String text) 
 {
+  if (!lcd_enabled) {
+    lcd_enabled = 1;
+    lcd.display();
+  }
+  lcd_update_time = millis();
+
   lcd.setCursor(col, row);
   lcd.print(text);
+}
+
+void LCD_loop() 
+{
+  if (lcd_disable_timeout &&
+      (millis() - lcd_update_time > lcd_disable_timeout)) {
+    /* Disable the display when inactive */
+    lcd_update_time = millis();
+    lcd_enabled = 0;
+    lcd.noDisplay();
+  }
 }
